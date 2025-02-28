f25c51c9abc92d079383738e1164a0c6f4fefc74
hnidesu <hnidesu@outlook.com>
2025-02-28 21:09:44 +0800

    Bump version to 1.2.0.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.hnidesu.timer"
         minSdk = 23
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
